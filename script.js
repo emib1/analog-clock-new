b24585e85c3757609b6e0709406e58653ac3df6c
emib1 <ibeman851@gmail.com>
2023-02-07 15:39:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+setInterval(() => {
+     d = new Date();
+     hr = d.getHours();
+     min = d.getMinutes();
+     sec = d.getSeconds();
+     hr_rotation = 30 * hr + min /2;
+     min_rotation = 6* min;
+     sec_rotation = 6 *sec;
+
+     hour = document.querySelector('#hour');
+     min = document.querySelector('#minute');
+     seconds = document.querySelector('#second')
+
+     hour.style.transform = `rotate(${hr_rotation}deg)`;
+     minute.style.transform = `rotate(${min_rotation}deg)`;
+     second.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);
